e26ac2aa112ea63630d233613ab6de5459aee84e
limhoontaig <limhoontaig@gmail.com>
2022-09-24 09:57:18 +0900

    Program to Print ASCII Value

---
+++ work_05.c
@@ -0,0 +1,17 @@
+/* Program to Print ASCII Value */
+
+#include <stdio.h>
+
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d\n", c, c);
+
+    return 0;
+
+}
+
