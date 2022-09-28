17df36d9b84988d3f8ea7c5c2ebdb28c1a3a3b99
limhoontaig <limhoontaig@gmail.com>
2022-09-28 09:42:00 +0900

    modified input sequence using if statement

--- work_47.c
+++ work_47.c
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <stdio.h>
 
+// function prototype
 float calculateSD(float data[]);
 
 int main() {
@@ -15,11 +16,16 @@ int main() {
     printf("Enter 10 elements: ");
     
     for (i = 0; i < 10; ++i) {
-	scanf("%f", &data[i]);
-        printf("Number %d: %.2f\n", i, data[i]);
+		scanf("%f", &data[i]);
+        printf("Number %d: %f\n", (i + 1), data[i]);
+		if (i < 9) {
+			printf("Next Number %d: ", i + 2);
+		} else {
+			printf("\nCaculate Standard Deviation:\n");
+		}
     }
     
-    printf("\nStandard Deviation = %.6f\n", calculateSD(data));
+    printf("Standard Deviation = %.6f\n", calculateSD(data));
     
     return 0;
 }
