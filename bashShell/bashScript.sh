ff4745b9b1922742593797d6255d6557e41a3dcf
limhoontaig <limhoontaig@gmail.com>
2022-10-03 14:19:54 +0900

    bash tutorials

---
+++ bashShell/bashScript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+declare -a ARRAY
+exec 10<&0
+exec < $1
+let count=0
+
+while read LINE; do
+	ARRAY[$count]=$LINE
+	((count++))
+done
+echo Number of Elements: ${#ARRAY[@]}
+echo ${ARRAY[@]}
+exec 0<&10 10<&-
