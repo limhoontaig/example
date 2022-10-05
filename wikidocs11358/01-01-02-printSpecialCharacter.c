04d91254d4feec75b8eaa7da61cfadb12be7ea96
limhoontaig <limhoontaig@gmail.com>
2022-10-05 15:48:13 +0900

    c tutorials wikidocs 11358 chap 1

---
+++ wikidocs11358/01-01-02-printSpecialCharacter.c
@@ -0,0 +1,12 @@
+/* 특수 문자 프린트 */
+
+#include <stdio.h>
+
+int main(void) {
+
+	printf("큰따옴표: \" \"\n");
+	printf("작은따옴표: \' \' \n");
+	printf("역슬래시: \\ \n");
+    
+	return 0;
+}
---
+++ wikidocs11358/01-01-04-print진수표시.c
@@ -0,0 +1,12 @@
+/* 진수 표기법 */
+
+#include <stdio.h>
+
+int main() {
+
+	printf("Formatter: 10진수: %%d, 16진수: %%x, 8진수: %%o\n");
+	printf("10진수: %d는 16진수: %x, 8진수: %o 입니다.\n", 50,50,50);
+
+	return 0;
+}
+
